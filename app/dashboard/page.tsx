0ee0c56fbca64640ea5647294582e879f4e758e8
jacobtt21 <jacobt1206@gmail.com>
2024-01-25 10:26:50 -0600

    sessions are working

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,17 @@
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/utils/auth"
+
 import LogoutButton from "@/components/specifics/authComponents/logoutButton"
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions)
+
   return (
     <main>
       <div className="h-96 w-full">
         <h1>Dashboard</h1>
+        <p>{session?.email}</p>
+        <p>{session?.name}</p>
         <LogoutButton />
       </div>
     </main>
