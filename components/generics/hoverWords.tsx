504cad7fb9d3d4e585e0f1fdf4b7f7abe7670fa6
jacobtt21 <jacobt1206@gmail.com>
2024-01-25 09:02:12 -0600

    changed colors

--- components/generics/hoverWords.tsx
+++ components/generics/hoverWords.tsx
@@ -1,5 +1,5 @@
 export default function HoverWords({ children } : { children: React.ReactNode }) {
   return (
-    <p className="text-slate-800 hover:text-black transition">{children}</p>
+    <p className="text-slate-600 hover:text-black transition">{children}</p>
   )
 }
