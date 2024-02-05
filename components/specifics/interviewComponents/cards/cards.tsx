913eeb822fc21662518aaf5f08298339873706c0
jacobtt21 <jacobt1206@gmail.com>
2024-02-05 17:55:30 -0600

    work on card

--- components/specifics/interviewComponents/cards/cards.tsx
+++ components/specifics/interviewComponents/cards/cards.tsx
@@ -5,17 +5,27 @@ import Link from "next/link"
 export default function Card({ interview } : { interview: interviewSavedInfo }) {
 
   return (
-    <Link href={`/dashboard/${interview.teamId}/${interview.id}`}>
-      <div className="border hover:border-slate-600 p-4 transition rounded shadow-sm">
-        <div className="text-xs">
-          <p className="text-xl font-medium">{interview.name}</p>
-          <p className="mt-2">{interview.purpose}</p>
-          <div className="flex justify-between mt-8 text-sm">
-            <p>{interview.transcript.length} Conducted</p>
-            <p>{interview.guide.length} Questions</p>
+    <>
+      <Link href={`/dashboard/${interview.teamId}/${interview.id}`}>
+        <div className="border hover:border-slate-600 p-4 transition rounded shadow-sm">
+          <div className="text-xs">
+            <p className="text-xl font-medium">{interview.name}</p>
+            <p className="mt-2 h-8 line-clamp">{interview.purpose}</p>
+            <div className="flex justify-between mt-8 text-sm">
+              <p>{interview.transcript.length} Responses</p>
+              <p>{interview.guide.length} Questions</p>
+            </div>
           </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+      <style jsx>{`
+        .line-clamp {
+          display: -webkit-box;
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: 2;
+          overflow: hidden;
+        }
+      `}</style>
+    </>
   )
 }
