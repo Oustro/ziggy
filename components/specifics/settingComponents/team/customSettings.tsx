d422327af542e3620551e986691d97b1055d2818
jacobtt21 <jacobt1206@gmail.com>
2024-02-10 17:07:23 -0600

    added colors and logos

--- components/specifics/settingComponents/team/customSettings.tsx
+++ components/specifics/settingComponents/team/customSettings.tsx
@@ -82,7 +82,7 @@ export default function CustomSettings({ team, setRefreshKey } : { team: teamSav
               alt="Team Logo"
               width={100}
               height={100}
-              className="mx-auto border rounded-full px-2 h-24 w-24"
+              className="mx-auto rounded"
               />
               <p className="text-xs mt-4 text-slate-600">Click to change logo</p>
             </div>
--- components/specifics/teamComponents/cards/card.tsx
+++ components/specifics/teamComponents/cards/card.tsx
@@ -1,6 +1,7 @@
 import { teamSavedInfo } from "@/lib/types"
 
 import Link from "next/link"
+import Image from "next/image"
 
 import Badge from "@/components/generics/badge"
 import TeamSettings from "@/components/specifics/settingComponents/teamSettings"
@@ -8,8 +9,12 @@ import BlackButton from "@/components/generics/blackButton"
 
 export default function Card({ team, open, setRefreshKey } : { team: teamSavedInfo, open: boolean, setRefreshKey: Function }) {
 
+  const mystyle={
+    background: 'linear-gradient(to top right, #FFFFFF 50%, '+team.color+' 190%)'
+  }
+
   return (
-    <div className="border hover:border-slate-600 p-4 transition rounded shadow-sm">
+    <div className="border hover:border-slate-600 p-4 transition rounded shadow-sm" style={mystyle}>
       <div className="flex text-xs justify-between items-start">
         <Badge>{team.plan === 0 ? "Free" : team.plan === 1 ? "Pro" : team.plan === 2 ? "Business" : "Enterprise"}</Badge>
         <TeamSettings initOpen={open} team={team} setRefreshKey={setRefreshKey}>
@@ -17,7 +22,16 @@ export default function Card({ team, open, setRefreshKey } : { team: teamSavedIn
         </TeamSettings>
       </div>
       <Link href={`/dashboard/${team.id}`}>
-        <p className="text-xl font-medium">{team.name}</p>
+        <div className="mt-4 flex items-center gap-2">
+          <Image
+          src={team.logo}
+          alt="Team Logo"
+          width={40}
+          height={40}
+          className="rounded"
+          />
+          <p className="text-xl font-medium">{team.name}</p>
+        </div>
         <div className="flex justify-between mt-8 text-sm">
           <p className="flex items-center gap-1">{team.interviews.length} Interviews</p>
           <p className="flex items-center gap-1">{team.members.length} Members</p>
