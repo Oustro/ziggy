1c24ec4b9581476dfd4feb7e4d3f319f85717443
jacobtt21 <jacobt1206@gmail.com>
2024-02-18 22:24:31 -0600

    added sentiment chart

---
+++ components/specifics/interviewComponents/dashboard/overviewComponents/sentiment.tsx
@@ -0,0 +1,50 @@
+import { Doughnut } from "react-chartjs-2"
+import { Chart, ArcElement } from 'chart.js'
+Chart.register(ArcElement)
+
+export default function Sentiment({ data } : { data: number[] }) {
+  return (
+    <div className="w-[50%] rounded p-4">
+      <h3 className="text-2xl font-medium">Sentiment Analysis</h3>
+      <p className="mt-2 text-sm">This metric shows the general sentiment interviewees answers have while responding to questions.</p>
+      <div className="flex mt-8 justify-center text-sm gap-6 font-medium">
+        <div className="rounded-full px-2 border border-green-500 bg-green-200">
+          <p>{data[0]} Positive</p>
+        </div>
+        <div className="rounded-full px-2 border bg-slate-200 border-slate-600">
+          <p>{data[1]} Neutral</p>
+        </div>
+        <div className="rounded-full px-2 border border-red-400 bg-red-200">
+          <p>{data[2]} Negative</p>
+        </div>
+      </div>
+      <div className="mt-8 w-full text-center">
+        <Doughnut
+        data={{
+          labels: ['Positive', 'Neutral', 'Negative'],
+          datasets: [{
+            data: data,
+            backgroundColor: ['#bbf7d0', '#e2e8f0', '#fecaca'],
+            borderRadius: 5,
+            borderWidth: 2,
+            spacing: 15,
+            hoverBackgroundColor: ['#bbf7d0', '#e2e8f0', '#fecaca'],
+            hoverBorderWidth: 2,
+            hoverBorderColor: ['#22c55e', '#475569', '#f43f5e'],
+            borderColor: ['#22c55e', '#475569', '#f43f5e'],
+          }]
+        }}
+        options={{
+          responsive: true,
+          plugins: {
+            legend: {
+              display: false
+            }
+          }
+        }}
+        className="mx-auto"
+        />
+      </div>
+    </div> 
+  )
+}
