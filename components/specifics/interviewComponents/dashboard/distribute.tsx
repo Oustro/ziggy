b3ba4b3a133d24e1715bcaaf489a39ac8089bc21
jacobtt21 <jacobt1206@gmail.com>
2024-02-27 15:54:55 -0600

    distro page

--- components/specifics/interviewComponents/dashboard/distribute.tsx
+++ components/specifics/interviewComponents/dashboard/distribute.tsx
@@ -28,11 +28,11 @@ export default function Distribute({ externalId, interviewName } : { externalId:
   return (
     <div>
       <h1 className="text-4xl font-semibold">Distribute</h1>
-      <div className="mt-8 text-center">
-        <h3>QR Code</h3>
-        <p></p>
-        <QRCode id="QRCode" className="rounded p-2 mx-auto border border-slate-600" value={window.location.origin + '/zy/'+externalId} />
-        <button className="mt-4" onClick={downloadQR}>
+      <div className="mt-8">
+        <h3 className="text-2xl font-medium">QR Code</h3>
+      <p className="mt-2 text-slate-600">Scan the this QR to take your Ziggy Interview. Great for printing and sharing too.</p>
+        <QRCode id="QRCode" className="rounded p-2 border border-slate-600 mt-6" value={window.location.origin + '/zy/'+externalId} />
+        <button className="mt-6" onClick={downloadQR}>
           <BlackButton>
             Download QR Code
           </BlackButton>
