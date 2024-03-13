93b394b1ca540d7c9b6cb2c7e676f260e0dffc86
jacobtt21 <jacobt1206@gmail.com>
2024-03-13 16:45:51 -0500

    redid

--- app/page.tsx
+++ app/page.tsx
@@ -3,48 +3,25 @@ import Footer from "@/components/specifics/navbars/footer"
 
 import Hero from "@/components/specifics/landingComponents/hero"
 
-import { FaMagnifyingGlass } from "react-icons/fa6"
-
 import Image from "next/image"
 
-import Link from "next/link"
+import { FaMagnifyingGlass } from "react-icons/fa6"
+
 
 export default function Home() {
 
   return (
     <main className="mb-16">
       <HomeNav />
-      <div className="p-8 flex gap-8">
-        <div className="w-[50%] flex h-[35rem] flex-col">
-          <div className="w-full rounded">
-            <h2 className="text-5xl font-medium">Better Feedback</h2>
-            <h1 className="text-6xl mt-4 font-bold">Starts with Ziggy</h1>
-            <p className="mt-8 text-lg text-slate-600">Use AI to conduct interviews so you can get better information than traditional survey forms and do it faster than sitting down and having 1:1 conversations.</p>
-          </div>
-          <div className="rounded justify-center flex-col w-full mt-8 flex-grow border border-slate-600 relative">
-            <div className="absolute top-0 w-full px-4 pt-4">
-              <div className="border-b border-slate-600 pb-1 flex items-center gap-2">
-                <FaMagnifyingGlass />
-                <p>Why do businesses love Ziggy?</p>
-              </div>
-            </div>
-            <div className="h-full p-4 flex items-center gap-4">
-              <Image
-              src="/ziggy-logo.svg"
-              alt="Ziggy logo"
-              priority
-              width={40}
-              height={40}
-              />
-              <p className="mt-4">
-                According to responses from interviews, businesses and entrepreneurs love Ziggy because feedback from 
-                users are much more natural and insightful than before. Additionally Ziggy's powerful and useful analytical
-                features make understaing feedback much easier. Users also appreciate that Ziggy is <span className="font-bold underline"><Link href="https://github.com/oustro/ziggy">100% open source.</Link></span>
-              </p>
-            </div>
-          </div>
+      <div className="w-full mt-12 text-center">
+        <div className="w-full rounded">
+          <h2 className="text-7xl font-medium">Better Feedback</h2>
+          <h1 className="text-8xl mt-4 font-bold">Starts with Ziggy</h1>
+          <p className="mt-8 w-[50%] mx-auto text-xl text-slate-600">Use AI to conduct interviews so you can get better information than traditional survey forms and do it faster than sitting down and having 1:1 conversations.</p>
         </div>
-        <Hero />
+        <div className="mt-24 px-24 relative text-left">
+          <Hero />
+        </div>
       </div>
       <Footer />
     </main>
--- components/specifics/landingComponents/hero.tsx
+++ components/specifics/landingComponents/hero.tsx
@@ -10,7 +10,16 @@ import Image from "next/image"
 
 import { motion, AnimatePresence } from "framer-motion"
 
+import { FaMagnifyingGlass } from "react-icons/fa6"
+
 import Link from "next/link"
+
+import { Line } from "react-chartjs-2"
+import { Chart, CategoryScale, PointElement, LinearScale, LineElement } from 'chart.js'
+Chart.register(CategoryScale)
+Chart.register(LinearScale)
+Chart.register(PointElement)
+Chart.register(LineElement)
 
 export default function Hero() {
 
@@ -67,7 +76,7 @@ export default function Hero() {
   const [activeConvo, setActiveConvo] = useState(0)
 
   return (
-    <div className="relative border flex flex-col h-[35rem] justify-center border-slate-600 rounded w-[50%]"
+    <div className="relative border flex flex-col h-[35rem] justify-center border-slate-600 rounded w-[55rem] mx-auto mt-8 shadow-2xl"
     style={{
       background: 'linear-gradient(to top right, #FFFFFF 40%, #8B5CF6 145%)'
     }}
@@ -100,6 +109,80 @@ export default function Hero() {
           </motion.div>
         </AnimatePresence>
         {convo[activeConvo].button}
+      </div>
+      <div className="absolute top-20 rotate-[19deg] -right-48 w-[30rem] border border-slate-600 bg-white rounded p-4">
+        <p className="text-lg font-medium">Key Trends</p>
+        <div className="grid grid-cols-2 gap-2 mt-4">
+          <div className="border rounded p-4">
+            <p className="text-base font-medium">Better Feedback</p>
+            <p className="text-xs mt-3 font-medium">Explore &rsaquo;</p>
+          </div>
+          <div className="border rounded p-4">
+            <p className="text-base font-medium">At Scale</p>
+            <p className="text-xs mt-3 font-medium">Explore &rsaquo;</p>
+          </div>
+        </div>
+      </div>
+      <div className="absolute -top-8 -rotate-[20deg] -left-36 w-96 border border-slate-600 bg-white rounded p-4">
+        <p className="text-lg font-medium">AI Q&A</p>
+        <div className="border-b border-slate-600 pb-1 flex items-center gap-2 mt-4">
+          <FaMagnifyingGlass />
+          <p>Why do businesses love Ziggy?</p>
+        </div>
+        <div className="mt-4 flex items-center gap-2">
+          <Image
+          src="/ziggy-logo.svg"
+          alt="Ziggy logo"
+          width={40}
+          height={40}
+          priority
+          />
+          <p>Based on reponses, businesses love Ziggy for a number of reasons...</p>
+        </div>
+      </div>
+      <div className="absolute -bottom-24 -rotate-[8deg] h-48 right-72 w-96 border border-slate-600 bg-white rounded p-4">
+        <p className="text-lg font-medium">Interview Activity</p>
+        <div className="mt-4 h-[80%] pb-4">
+          <Line
+          data={{
+            labels: ['3/18', '3/19', '3/20', '3/21', '3/22', '3/23', '3/24'],
+            datasets: [{
+              label: 'My First Dataset',
+              data: [30, 50, 20, 30, 90, 120, 190],
+              fill: true,
+              animation: false,
+              borderWidth: 2,
+              borderColor: '#475569',
+              tension: 0.09
+            }]
+          }}
+          options={{
+            responsive: true,
+            maintainAspectRatio: false,
+            elements: {
+              point: {
+                radius: 0
+              }
+            },
+            scales: {
+              x: {
+                display: true,
+              },
+              y: {
+                display: true,
+              },
+            },
+            plugins: {
+              legend: {
+                display: false,
+              },
+              tooltip: {
+                enabled: true
+              },
+            },
+          }}
+          />
+        </div>
       </div>
     </div>
   )
