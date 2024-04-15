008880ebd3b97a00518f9e578361104136ad8ecb
jacobtt21 <jacobt1206@gmail.com>
2024-04-15 15:03:47 -0500

    verification route

---
+++ app/api/verification/route.ts
@@ -0,0 +1,51 @@
+import { NextResponse, NextRequest } from "next/server";
+import { verifyKey } from "@unkey/api";
+
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+export async function POST(request: NextRequest) {
+  const redis = new Redis({
+    url: process.env.UPSTASH_URL || "",
+    token: process.env.UPSTASH_TOKEN || "",
+  });
+
+  const ratelimit = new Ratelimit({
+    redis: redis,
+    limiter: Ratelimit.slidingWindow(10, "10 s"),
+  });
+
+  const identifier = "Token Verification API";
+  const { success } = await ratelimit.limit(identifier);
+
+  if (!success) {
+    return NextResponse.json(
+      { message: "Rate limit exceeded" },
+      { status: 429 }
+    );
+  }
+
+  const verificationToken = (await request.json()) as {
+    token: string;
+  };
+
+  try {
+    const { result, error } = await verifyKey({
+      key: verificationToken.token,
+      apiId: process.env.UNKEY_API_ID as string,
+    });
+
+    if (error) {
+      return NextResponse.json({ status: 503 });
+    }
+
+    if (!result.valid) {
+      return NextResponse.json({ status: 406 });
+    }
+
+    return NextResponse.json({ status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "error" }, { status: 500 });
+  }
+}
